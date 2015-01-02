cbe15ecc8a7522ff6d7158cb6e2181216dc5f59e
Naftan <DevNaftan@gmail.com>
2015-01-01 19:31:20 -0500

    Añadido el sistema de censura

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -1265,6 +1265,7 @@ void AddBattlegroundScripts()
 #ifdef SCRIPTS
 /* This is where custom scripts' loading functions should be declared. */
 
+void AddSC_CensureSystem();
 #endif
 
 void AddCustomScripts()
@@ -1272,5 +1273,6 @@ void AddCustomScripts()
 #ifdef SCRIPTS
     /* This is where custom scripts should be added. */
 
+    AddSC_CensureSystem();
 #endif
 }
